72dc6fda3a3b410ce06e0b6fa80a86b561c4fdae
altock <sjs382@cornell.edu>
2017-03-01 13:47:29 -0500

    Dev branch

---
+++ pelicanconf.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Sam "Sven" Svenningsen'
+SITENAME = "Sven's Blog"
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+#LINKS = (('Pelican', 'http://getpelican.com/'),
+#         ('Python.org', 'http://python.org/'),
+#         ('Jinja2', 'http://jinja.pocoo.org/'),
+#         ('You can modify those links in your config file', '#'),)
+
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+THEME = 'pelican-blue'
+
+# Social widget
+SOCIAL = (#('linkedin', 'https://www.linkedin.com/in/username'),
+          ('github', 'https://github.com/altock'),
+          #('twitter', 'https://twitter.com/username'),
+          )
+
+MARKUP = ('md', 'ipynb')
+
+PLUGIN_PATHS = ['./plugins']
+#PLUGINS = ['ipynb.markup']
